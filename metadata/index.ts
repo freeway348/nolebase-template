ff331c51a24e933cdbb0976d4c2c9ec22b70f315
note <chen1656497721@outlook.com>
2024-09-03 17:33:07 +0800

    restore

--- metadata/index.ts
+++ metadata/index.ts
@@ -7,11 +7,11 @@ export const siteDescription = '记录回忆，知识和畅想的地方'
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/freeway348/nolebase-template'
+export const githubRepoLink = 'https://github.com/nolebase/nolebase'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase-template-freeway348s-projects.vercel.app'
+export const plainTargetDomain = 'nolebase.ayaka.io'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
